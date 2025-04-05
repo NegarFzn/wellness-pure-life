2124b300029b41c4fe8d5af38abd358e0c3b4843
negar <negar.fozooni@gmail.com>
2025-04-05 08:50:23 +0300

    05April

--- components/DailyList/DailyList.js
+++ components/DailyList/DailyList.js
@@ -1,59 +1,43 @@
-import React, { useMemo } from "react";
-import FitnessItem from "../fitness/fitness-item";
-import MindfulnessItem from "../mindfulness/mindfulness-item";
-import NourishItem from "../nourish/nourish-item";
+import React from "react";
 import fitnessData from "../../data/fitness.json";
 import mindfulnessData from "../../data/mindfulness.json";
 import nourishData from "../../data/nourish.json";
+import FitnessItem from "../fitness/fitness-item";
+import MindfulnessItem from "../mindfulness/mindfulness-item";
+import NourishItem from "../nourish/nourish-item";
+import classes from "./DailyList.module.css";
 
-const DailyList = () => {
-  const dailyItems = useMemo(() => {
-    const seed = new Date().toDateString();
-    const seededRandom = (seed) => {
-      let h = 0;
-      for (let i = 0; i < seed.length; i++) {
-        h = (Math.imul(31, h) + seed.charCodeAt(i)) | 0;
-      }
-      return () => {
-        h ^= h << 13;
-        h ^= h >>> 17;
-        h ^= h << 5;
-        return (h >>> 0) / 4294967296;
-      };
-    };
+const allItems = [
+  ...Object.values(fitnessData).flat().map((item) => ({ ...item, type: "fitness" })),
+  ...Object.values(mindfulnessData).flat().map((item) => ({ ...item, type: "mindfulness" })),
+  ...Object.values(nourishData).flat().map((item) => ({ ...item, type: "nourish" }))
+];
 
-    const random = seededRandom(seed);
-    const allItems = [
-      ...Object.values(fitnessData).flat(),
-      ...Object.values(mindfulnessData).flat(),
-      ...Object.values(nourishData).flat(),
-    ];
+const getTodayItems = () => {
+  const seed = new Date().toDateString();
+  const hash = [...seed].reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  const shuffled = [...allItems].sort(() => 0.5 - (hash % 5) / 5);
+  return shuffled.slice(0, 2);
+};
 
-    const shuffled = [...allItems].sort(() => random() - 0.5);
-    return shuffled.slice(0, 2);
-  }, []);
+export default function DailyList() {
+  const todayItems = getTodayItems();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        gap: "2rem",
-        flexWrap: "wrap",
-        marginTop: "2rem",
-      }}
-    >
-      {dailyItems.map((item) => {
+    <div className={classes.dailyContainer}>
+      {todayItems.map((item) => {
         const Component =
-          item.category === "fitness"
+          item.type === "fitness"
             ? FitnessItem
-            : item.category === "mindfulness"
+            : item.type === "mindfulness"
             ? MindfulnessItem
             : NourishItem;
-        return <Component key={item.id} {...item} />;
+        return (
+          <div className={classes.cardWrapper} key={item.id}>
+            <Component {...item} />
+          </div>
+        );
       })}
     </div>
   );
-};
-
-export default DailyList;
+} 
--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,6 @@ export default function Home() {
                   
                 </div>
               ))}
-             
             </div>
             <DailyList />
           </section>
