8a4868f58444e60a13fdbab0c6438de03da04ab3
negar <negar.fozooni@gmail.com>
2025-08-06 10:10:11 +0300

    6Agusttt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: "https://yourdomain.com",
+  siteUrl: "https://wellnesspurelife.com",
   generateRobotsTxt: true,
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
+// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
 
 class MyDocument extends Document {
   render() {
@@ -12,6 +12,22 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6324625824043093"
             crossOrigin="anonymous"
           ></script>
+
+          {/* ✅ SEO Meta Tags */}
+          <meta name="robots" content="index, follow" />
+          <meta
+            name="description"
+            content="Wellness Pure Life offers expert advice on fitness, mindfulness, and nutrition to help you live a healthier life."
+          />
+          <meta
+            name="keywords"
+            content="wellness, fitness, mindfulness, nutrition, healthy living"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          {/* Optional: favicon or other link tags can go here */}
         </Head>
         <body>
           <div id="overlays" />
